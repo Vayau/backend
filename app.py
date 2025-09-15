4dd92da3bec4697fe92a53c465182ce56df7b0be
ifrah-ashraf <ifrahashraf48@gmail.com>
2025-09-15 11:00:14 +0530

    feat: added signup and login route with supabase

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from routes.auth_routes import auth_bp
 
 app = Flask(__name__)
+
+# this blueprint will help us to organise the routes properly
+app.register_blueprint(auth_bp, url_prefix="/auth")
 
 @app.route("/test")
 def test():
